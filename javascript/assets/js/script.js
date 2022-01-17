22c51d358d9f407ef51a9228c704993471f3418f
yuram2 <97070060+yuram2@users.noreply.github.com>
2022-01-17 10:08:04 +0900

    2022.01.17

---
+++ javascript/assets/js/script.js
@@ -0,0 +1,61 @@
+//결과보기
+//제목을 클릭했을 때 ~~~실행해라
+document.querySelector(".result01t h4").addEventListener("click", function(){
+    document.querySelector(".result01").style.display = "block";
+})
+
+document.querySelector(".result02t h4").addEventListener("click", function(){
+    document.querySelector(".result02").style.display = "block";
+})
+
+document.querySelector(".result03t h4").addEventListener("click", function(){
+    document.querySelector(".result03").style.display = "block";
+})
+
+document.querySelector(".result04t h4").addEventListener("click", function(){
+    document.querySelector(".result04").style.display = "block";
+})
+
+document.querySelector(".result05t h4").addEventListener("click", function(){
+    document.querySelector(".result05").style.display = "block";
+})
+
+document.querySelector(".result06t h4").addEventListener("click", function(){
+    document.querySelector(".result06").style.display = "block";
+})
+
+document.querySelector(".result07t h4").addEventListener("click", function(){
+    document.querySelector(".result07").style.display = "block";
+})
+
+document.querySelector(".result08t h4").addEventListener("click", function(){
+    document.querySelector(".result08").style.display = "block";
+})
+
+document.querySelector(".result09t h4").addEventListener("click", function(){
+    document.querySelector(".result09").style.display = "block";
+})
+
+document.querySelector(".result10t h4").addEventListener("click", function(){
+    document.querySelector(".result10").style.display = "block";
+})
+
+document.querySelector(".result11t h4").addEventListener("click", function(){
+    document.querySelector(".result11").style.display = "block";
+})
+
+document.querySelector(".result12t h4").addEventListener("click", function(){
+    document.querySelector(".result12").style.display = "block";
+})
+
+document.querySelector(".result13t h4").addEventListener("click", function(){
+    document.querySelector(".result13").style.display = "block";
+})
+
+document.querySelector(".result14t h4").addEventListener("click", function(){
+    document.querySelector(".result14").style.display = "block";
+})
+
+document.querySelector(".result15t h4").addEventListener("click", function(){
+    document.querySelector(".result15").style.display = "block";
+})
