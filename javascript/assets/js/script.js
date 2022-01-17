ed9afe4d4aee990a407a1d59eeee6574449eb8e9
yuram2 <97070060+yuram2@users.noreply.github.com>
2022-01-17 20:53:05 +0900

    2022.01.17

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,39 +1,39 @@
 //결과보기
 //제목을 클릭했을 때 ~~~실행해라
-document.querySelector(".result01t h4").addEventListener("click", function(){
-    document.querySelector(".result01").style.display = "block";
+document.querySelector(".result1t h4").addEventListener("click", function(){
+    document.querySelector(".result1").style.display = "block";
 })
 
-document.querySelector(".result02t h4").addEventListener("click", function(){
-    document.querySelector(".result02").style.display = "block";
+document.querySelector(".result2t h4").addEventListener("click", function(){
+    document.querySelector(".result2").style.display = "block";
 })
 
-document.querySelector(".result03t h4").addEventListener("click", function(){
-    document.querySelector(".result03").style.display = "block";
+document.querySelector(".result3t h4").addEventListener("click", function(){
+    document.querySelector(".result3").style.display = "block";
 })
 
-document.querySelector(".result04t h4").addEventListener("click", function(){
-    document.querySelector(".result04").style.display = "block";
+document.querySelector(".result4t h4").addEventListener("click", function(){
+    document.querySelector(".result4").style.display = "block";
 })
 
-document.querySelector(".result05t h4").addEventListener("click", function(){
-    document.querySelector(".result05").style.display = "block";
+document.querySelector(".result5t h4").addEventListener("click", function(){
+    document.querySelector(".result5").style.display = "block";
 })
 
-document.querySelector(".result06t h4").addEventListener("click", function(){
-    document.querySelector(".result06").style.display = "block";
+document.querySelector(".result6t h4").addEventListener("click", function(){
+    document.querySelector(".result6").style.display = "block";
 })
 
-document.querySelector(".result07t h4").addEventListener("click", function(){
-    document.querySelector(".result07").style.display = "block";
+document.querySelector(".result7t h4").addEventListener("click", function(){
+    document.querySelector(".result7").style.display = "block";
 })
 
-document.querySelector(".result08t h4").addEventListener("click", function(){
-    document.querySelector(".result08").style.display = "block";
+document.querySelector(".result8t h4").addEventListener("click", function(){
+    document.querySelector(".result8").style.display = "block";
 })
 
-document.querySelector(".result09t h4").addEventListener("click", function(){
-    document.querySelector(".result09").style.display = "block";
+document.querySelector(".result9t h4").addEventListener("click", function(){
+    document.querySelector(".result9").style.display = "block";
 })
 
 document.querySelector(".result10t h4").addEventListener("click", function(){
@@ -59,3 +59,20 @@ document.querySelector(".result14t h4").addEventListener("click", function(){
 document.querySelector(".result15t h4").addEventListener("click", function(){
     document.querySelector(".result15").style.display = "block";
 })
+
+//for문을 이용해서 1~12까지 출력
+
+// for(let i=1; i<=12; i++){
+//     document.querySelector(".result"+i+"t h4").addEventListener("click", function(){
+//         document.querySelector(".result" + i).style.display = "block";
+//     });
+// }
+
+
+//forEach()을 이용해서 1~12까지 출력
+// document.querySelectorAll(".result h4").forEach(function(el){
+//     // console.log(el)
+//     el.addEventListener("click", function(){
+//         alert("클릭");
+//     })
+// });
