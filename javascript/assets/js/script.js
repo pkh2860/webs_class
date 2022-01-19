603e16c48637520f6707f3edc2ddc3ec749d0956
yuram2 <97070060+yuram2@users.noreply.github.com>
2022-01-19 17:44:25 +0900

    2022.01.19

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,6 +1,8 @@
 //결과보기 버튼을 클릭하면 경고창을 띄우기
 document.querySelectorAll(".result").forEach(function(el){
-    el.classList.add(".result")
+    el.addEventListener("click", function(){
+        el.classList.toggle("active");
+    })
 });
 
 
