3a9fdd402049f7dfb752d23eb9901949724374ce
bonda1 <qnddj310@gmail.com>
2022-02-22 20:09:59 +0900

    2022.02.22

--- javascript/effect/js/custom.js
+++ javascript/effect/js/custom.js
@@ -45,7 +45,47 @@ const tabMenu = function(){
 
 
 
-         //클릭하지 않은건 active 삭제
+   
+
+
+
+    const modal2  = function(){
+        let windowWrapper = document.querySelector('#modal');                   //#modal을 windowWrapper에 저장
+        document.querySelector(".source_btn").addEventListener("click",()=>{ //source_btn을 클릭하면
+            document.querySelector("#modal").classList.add("show")          //modal에 show를 추가하고
+            document.querySelector("#modal").classList.remove("hide")        //hide를 삭제
+        })
+        document.querySelector("#modal-close").addEventListener("click", ()=>{      //modal-close를 클릭하면
+            document.querySelector("#modal").classList.add('hide');          //hide를 추가
+        })
+    }
+
+
+    const tabMenu2 = function(){
+        var tabBtn = document.querySelectorAll(".menu-bar > ul > li");
+        var tabCont = document.querySelectorAll(".content > div");
+
+        tabBtn.forEach((btn, index)=>{
+            btn.addEventListener("click", ()=>{
+                 //모든 클래스 active 삭제
+            tabBtn.forEach((el)=>{
+                el.classList.remove("active");
+            })
+            //내가 클릭한건 active 추가
+            btn.classList.add("active");
+
+            //모든 컨텐츠 박스 안보이게
+            tabCont.forEach((el)=>{
+                el.style.display = "none";
+            })
+            //내가 클릭한 콘텐츠 박스를 보이게
+            tabCont[index].style.display ="block";
+            })
+        })
+    }
+
+
+          //클릭하지 않은건 active 삭제
         // for(let btn of tabBtn){
         //     btn.classList.remove("active");
         // }
