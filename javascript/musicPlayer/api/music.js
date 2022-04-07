93419d05cfd5ad672dbca821f5df617b5a10cb5b
Pkh2860 <rmsgur2275@gmail.com>
2022-04-07 12:50:17 +0900

    2022.04.07

---
+++ javascript/musicPlayer/api/music.js
@@ -0,0 +1,122 @@
+let allMusic = [
+    {
+        name : "sailing",
+        artist : "Telecasted",
+        img : "music-1",
+        audio : "music-1"
+    },
+    {
+        name : "Breeze",
+        artist : "Telecasted",
+        img : "music-2",
+        audio : "music-2"
+    },
+    {
+        name : "Seasons",
+        artist : "Telecasted",
+        img : "music-3",
+        audio : "music-3"
+    },
+    {
+        name : "Campfire",
+        artist : "Telecasted",
+        img : "music-4",
+        audio : "music-4"
+    },
+    {
+        name : "Cattle",
+        artist : "Telecasted",
+        img : "music-5",
+        audio : "music-5"
+    },
+    {
+        name : "Rooster",
+        artist : "Telecasted",
+        img : "music-6",
+        audio : "music-6"
+    },
+    {
+        name : "Crops",
+        artist : "Telecasted",
+        img : "music-7",
+        audio : "music-7"
+    },
+    {
+        name : "County Lines",
+        artist : "Telecasted",
+        img : "music-8",
+        audio : "music-8"
+    },
+    {
+        name : "Thunder",
+        artist : "Telecasted",
+        img : "music-9",
+        audio : "music-9"
+    },
+    {
+        name : "Sunshine",
+        artist : "Telecasted",
+        img : "music-10",
+        audio : "music-10"
+    },
+    {
+        name : "Till I Let Go (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-11",
+        audio : "music-11"
+    },
+    {
+        name : "The Itch (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-12",
+        audio : "music-12"
+    },
+    {
+        name : "Don't Wanna Let Myself Down (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-13",
+        audio : "music-13"
+    },
+    {
+        name : "That's What It Takes (Instrumental)",
+        artist : "NEFFEX",
+        img : "music-14",
+        audio : "music-14"
+    },
+    {
+        name : "Yummu",
+        artist : "Leviathe",
+        img : "music-15",
+        audio : "music-15"
+    },
+    {
+        name : "Yucatan Peninsula",
+        artist : "Biz Baz Studio",
+        img : "music-16",
+        audio : "music-16"
+    },
+    {
+        name : "Yu-City Kamata",
+        artist : "Dan Bodan",
+        img : "music-17",
+        audio : "music-17"
+    },
+    {
+        name : "Your Voice Is American",
+        artist : "Letter Box",
+        img : "music-18",
+        audio : "music-18"
+    },
+    {
+        name : "Your Intro",
+        artist : "The Whole Other",
+        img : "music-19",
+        audio : "music-19"
+    },
+    {
+        name : "Young Squire",
+        artist : "TrackTribe",
+        img : "music-20",
+        audio : "music-20"
+    },
+]
