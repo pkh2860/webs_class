0b07cd3b77c5274368cc3ef59d521b1ef084eff8
Pkh2860 <rmsgur2275@gmail.com>
2022-04-08 13:57:15 +0900

    2022.04.08

--- javascript/musicPlayer/api/music.js
+++ javascript/musicPlayer/api/music.js
@@ -1,120 +1,120 @@
 let allMusic = [
     {
-        name : "sailing",
+        name : "1. sailing",
         artist : "Telecasted",
         img : "music-1",
         audio : "music-1"
     },
     {
-        name : "Breeze",
+        name : "2. Breeze",
         artist : "Telecasted",
         img : "music-2",
         audio : "music-2"
     },
     {
-        name : "Seasons",
+        name : "3. Seasons",
         artist : "Telecasted",
         img : "music-3",
         audio : "music-3"
     },
     {
-        name : "Campfire",
+        name : "4. Campfire",
         artist : "Telecasted",
         img : "music-4",
         audio : "music-4"
     },
     {
-        name : "Cattle",
+        name : "5. Cattle",
         artist : "Telecasted",
         img : "music-5",
         audio : "music-5"
     },
     {
-        name : "Rooster",
+        name : "6. Rooster",
         artist : "Telecasted",
         img : "music-6",
         audio : "music-6"
     },
     {
-        name : "Crops",
+        name : "7. Crops",
         artist : "Telecasted",
         img : "music-7",
         audio : "music-7"
     },
     {
-        name : "County Lines",
+        name : "8. County Lines",
         artist : "Telecasted",
         img : "music-8",
         audio : "music-8"
     },
     {
-        name : "Thunder",
+        name : "9. Thunder",
         artist : "Telecasted",
         img : "music-9",
         audio : "music-9"
     },
     {
-        name : "Sunshine",
+        name : "10. Sunshine",
         artist : "Telecasted",
         img : "music-10",
         audio : "music-10"
     },
     {
-        name : "Till I Let Go (Instrumental)",
+        name : "11. Till I Let Go (Instrumental)",
         artist : "NEFFEX",
         img : "music-11",
         audio : "music-11"
     },
     {
-        name : "The Itch (Instrumental)",
+        name : "12. The Itch (Instrumental)",
         artist : "NEFFEX",
         img : "music-12",
         audio : "music-12"
     },
     {
-        name : "Don't Wanna Let Myself Down (Instrumental)",
+        name : "13. Don't Wanna Let Myself Down (Instrumental)",
         artist : "NEFFEX",
         img : "music-13",
         audio : "music-13"
     },
     {
-        name : "That's What It Takes (Instrumental)",
+        name : "14. That's What It Takes (Instrumental)",
         artist : "NEFFEX",
         img : "music-14",
         audio : "music-14"
     },
     {
-        name : "Yummu",
+        name : "15. Yummu",
         artist : "Leviathe",
         img : "music-15",
         audio : "music-15"
     },
     {
-        name : "Yucatan Peninsula",
+        name : "16. Yucatan Peninsula",
         artist : "Biz Baz Studio",
         img : "music-16",
         audio : "music-16"
     },
     {
-        name : "Yu-City Kamata",
+        name : "17. Yu-City Kamata",
         artist : "Dan Bodan",
         img : "music-17",
         audio : "music-17"
     },
     {
-        name : "Your Voice Is American",
+        name : "18. Your Voice Is American",
         artist : "Letter Box",
         img : "music-18",
         audio : "music-18"
     },
     {
-        name : "Your Intro",
+        name : "19. Your Intro",
         artist : "The Whole Other",
         img : "music-19",
         audio : "music-19"
     },
     {
-        name : "Young Squire",
+        name : "20. Young Squire",
         artist : "TrackTribe",
         img : "music-20",
         audio : "music-20"
