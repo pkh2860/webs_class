1dfade1076a46210278e8171dd5d4cfdf4db15a6
pkh2860 <97070047+pkh2860@users.noreply.github.com>
2022-06-03 14:16:26 +0900

    2022.06.03

--- portfolio/assets/js/slider.js
+++ portfolio/assets/js/slider.js
@@ -1,125 +1,3 @@
-// const lerp = (f0, f1, t) => (1 - t) * f0 + t * f1;
-// const clamp = (val, min, max) => Math.max(min, Math.min(val, max));
-
-// class DragScroll {
-//   constructor(obj) {
-//     this.$el = document.querySelector(obj.el);
-//     this.$wrap = this.$el.querySelector(obj.wrap);
-//     this.$items = this.$el.querySelectorAll(obj.item);
-//     this.init();
-//   }
-
-//   init() {
-//     this.progress = 0;
-//     this.speed = 0;
-//     this.oldX = 0;
-//     this.x = 0;
-//     this.playrate = 0;
-//     this.bindings();
-//     this.events();
-//     this.calculate();
-//     this.raf();
-//   }
-
-//   bindings() {
-//     [
-//       "events",
-//       "calculate",
-//       "raf",
-//       "move",
-//       "raf",
-//       "handleTouchStart",
-//       "handleTouchMove",
-//       "handleTouchEnd",
-//     ].forEach((i) => {
-//       this[i] = this[i].bind(this);
-//     });
-//   }
-
-//   calculate() {
-//     this.progress = 0;
-//     this.wrapWidth = this.$items[0].clientWidth * this.$items.length;
-//     this.$wrap.style.width = `${this.wrapWidth}px`;
-//     this.maxScroll = this.wrapWidth - this.$el.clientWidth;
-//   }
-
-//   handleTouchStart(e) {
-//     e.preventDefault();
-//     this.dragging = true;
-//     this.startX = e.clientX || e.touches[0].clientX;
-//     this.$el.classList.add("dragging");
-//   }
-
-//   handleTouchMove(e) {
-//     if (!this.dragging) return false;
-//     const x = e.clientX || e.touches[0].clientX;
-//     this.progress += (this.startX - x) * 2.5;
-//     this.startX = x;
-//     this.move();
-//   }
-
-//   handleTouchEnd() {
-//     this.dragging = false;
-//     this.$el.classList.remove("dragging");
-//   }
-
-//   move() {
-
-//     if(matchMedia("screen and (min-width:1500px)").matches){
-//       this.progress = clamp(this.progress, -700, this.maxScroll);
-//       this.maxScroll = clamp(this.progress, +700, this.maxScroll);
-//     } else {
-//       this.progress = clamp(this.progress, -250, this.maxScroll);
-//       this.maxScroll = clamp(this.progress, +250, this.maxScroll);
-//     }
-//   }
-
-//   events() {
-//     window.addEventListener("resize", this.calculate);
-
-//     this.$el.addEventListener("touchstart", this.handleTouchStart);
-//     window.addEventListener("touchmove", this.handleTouchMove);
-//     window.addEventListener("touchend", this.handleTouchEnd);
-
-//     window.addEventListener("mousedown", this.handleTouchStart);
-//     window.addEventListener("mousemove", this.handleTouchMove);
-//     window.addEventListener("mouseup", this.handleTouchEnd);
-//     document.body.addEventListener("mouseleave", this.handleTouchEnd);
-//   }
-
-//   raf() {
-
-//     this.x = lerp(this.x, this.progress, 0.1);
-//     this.playrate = this.x / this.maxScroll;
-
-//     this.$wrap.style.transform = `translateX(${-this.x}px)`;
-
-//     this.speed = Math.min(100, this.oldX - this.x);
-//     this.oldX = this.x;
-
-//     this.scale = lerp(this.scale, this.speed, 0.1);
-//     this.$items.forEach((i) => {
-//       i.style.transform = `scale(${1 - Math.abs(this.speed) * 0.002})`;
-//       i.querySelector("img").style.transform = `scaleX(${
-//         1 + Math.abs(this.speed) * 0.004
-//       })`;
-//     });
-//   }
-// }
-
-// const scroll = new DragScroll({
-//   el: ".carousel",
-//   wrap: ".carousel--wrap",
-//   item: ".carousel--item",
-
-// });
-
-// const raf = () => {
-//   requestAnimationFrame(raf);
-//   scroll.raf();
-// };
-// raf();
-
 function _getClosest(item, array, getDiff) {
   var closest, diff;
 
@@ -196,7 +74,8 @@ class Slider {
     this.max = -(this.sliderWidth + window.innerWidth);
 
     this.slides.forEach((slide, index) => {
-      slide.style.left = `${-650 + index * slideWidth}px`;
+      // slide.style.left = `${index * slideWidth}px`;
+      slide.style.left = `${-500 + index * slideWidth}px`;
     });
   }
 
@@ -243,7 +122,7 @@ class Slider {
     this.slides.forEach((slide, index) => {
       const bounds = slide.getBoundingClientRect();
       const diff = this.currentX - this.lastX;
-      const center = bounds.x + diff + bounds.width / 2;
+      const center = bounds.x + diff + bounds.width / 2 ;
       const fromCenter = this.centerX - center;
       numbers.push(fromCenter);
     });
